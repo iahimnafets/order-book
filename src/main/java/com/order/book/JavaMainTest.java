674cc594d5692bc160faff7c1b0583a53eb761c7
mihaistefan <mihai.sacalul@gmail.com>
2023-03-16 15:05:49 +0100

    Added some record for test !

--- src/main/java/com/order/book/JavaMainTest.java
+++ src/main/java/com/order/book/JavaMainTest.java
@@ -13,16 +13,20 @@ public class JavaMainTest {
 
         // Add some orders
         orderBook.addOrder(new Order(1, 100.0, 'B', 10));
-        orderBook.addOrder(new Order(2, 99.5, 'O', 5));
+        orderBook.addOrder(new Order(8, 101.1, 'B', 7));
         orderBook.addOrder(new Order(3, 101.0, 'B', 7));
-        orderBook.addOrder(new Order(4, 98.0, 'O', 3));
         orderBook.addOrder(new Order(5, 100.5, 'B', 2));
+        orderBook.addOrder(new Order(2, 99.5, 'O', 5));
+        orderBook.addOrder(new Order(4, 98.0, 'O', 3));
+        orderBook.addOrder(new Order(6, 93.0, 'O', 4));
+        orderBook.addOrder(new Order(7, 93.0, 'O', 3));
 
         // get some result from methods
-        System.out.println("Best bid price: " + orderBook.getPrice('B', 1));
-        System.out.println("Total size available at best bid: " + orderBook.getTotalSize('B', 1));
-        System.out.println("Second best offer price: " + orderBook.getPrice('O', 2));
-        System.out.println("Total size available at third best bid: " + orderBook.getTotalSize('B', 3));
+        System.out.println("=> Best bid price: " + orderBook.getPrice('B', 1));
+        System.out.println("=> Total size available at best bid: " + orderBook.getTotalSize('B', 1));
+        System.out.println("=> Second best offer price: " + orderBook.getPrice('O', 2));
+        System.out.println("=> Total size available at third best bid: " + orderBook.getTotalSize('B', 3));
+        System.out.println("=> Best offer price: " + orderBook.getPrice('O', 1));
 
         List<Order> bids = orderBook.getOrders('B');
         List<Order> offer = orderBook.getOrders('O');
@@ -31,13 +35,13 @@ public class JavaMainTest {
 
         // Modify an existing order
         orderBook.modifyOrderSize(1, 5);
-        System.out.println("Size of order 1 after modification: " + orderBook.getOrder(1).getSize());
+        System.out.println("=> Size of order 1 after modification: " + orderBook.getOrder(1).getSize());
 
         // Remove an order
         orderBook.removeOrder(2);
-        System.out.println("Order with id 2 removed");
+        System.out.println("=> Order with id 2 removed");
 
-        System.out.println(" After some updates in the system  \n");
+        System.out.println("=>  After some updates in the system  \n");
 
         bids = orderBook.getOrders('B');
         offer = orderBook.getOrders('O');
@@ -47,9 +51,9 @@ public class JavaMainTest {
     }
 
     private static void printOrders(List<Order> orders, String type  ){
-        System.out.println("\nAll " + type + " in the book:");
+        System.out.println("\n All " + type + " in the book:");
         for (Order bid : orders) {
-            System.out.println("id: "+bid.getId() + " Price: " + bid.getPrice() + " Size: " + bid.getSize());
+            System.out.println("-> id: "+bid.getId() + " Price: " + bid.getPrice() + " Size: " + bid.getSize());
         }
     }
 }
